92e7f287571fdb5af57bb9282ca9d6f37acbd64f
bhavishya <bhavisachdeva3102@gmail.com>
2024-12-30 12:24:27 +0530

    add search bar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const dropdownRef = useRef(null);
 
   const mensWearCollections = [
@@ -52,34 +53,46 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-[#E6E8FA] text-black shadow-md  ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+    <nav className="bg-[#E6E8FA] text-black shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-         
+     
           <div className="flex-shrink-0">
             <a href="/" className="text-2xl font-bold">
               Company Logo
             </a>
           </div>
 
-         
-          <div className="hidden md:flex space-x-4">
+        
+
+       
+          <div className="hidden md:flex items-center space-x-4">
+
+           
+          
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search here..."
+              className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#c1c1ff] text-black"
+            />
+      
             <div className="relative" ref={dropdownRef}>
               <button
                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                className="hover:text-black focus:outline-none"
+                className="hover:text-black focus:outline-none font-semibold"
               >
                 Collections
               </button>
 
-             
               {isDropdownOpen && (
-                <div className="absolute mt-2 w-48  text-black rounded-md shadow-lg z-20 bg-[#E6E8FA]">
+                <div className="absolute mt-2 w-48 text-black rounded-md shadow-lg z-20 bg-[#E6E8FA]">
                   {mensWearCollections.map((collection) => (
                     <a
                       key={collection.id}
                       href={collection.link}
-                      className="block px-4 py-2 hover:text-black hover:bg-[#cac6c6] "
+                      className="block px-4 py-2 hover:text-black hover:bg-[#f8e8e8]"
                     >
                       {collection.name}
                     </a>
@@ -92,7 +105,7 @@ const Navbar = () => {
         
           <div className="md:hidden">
             <button
-              className=" hover:text-black focus:outline-none"
+              className="hover:text-black focus:outline-none"
               onClick={() => setIsDropdownOpen(!isDropdownOpen)}
             >
               ☰
