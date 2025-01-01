01b6db38efcd1a49825091f4d633133c0988247f
bhavishya <bhavisachdeva3102@gmail.com>
2025-01-01 23:39:40 +0530

    responsive

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -31,7 +31,7 @@ const CardSlider: React.FC = () => {
           {cards.map((card) => (
             <div
               key={card.id}
-              className="flex-none w-1/4 p-4 "
+              className="flex-none w-1/2 md:w-1/3 lg:w-1/4 p-4 "
             >
               <div className="bg-white rounded-lg shadow-lg overflow-hidden border-solid border-4 border-navbarBG">
                 <img
