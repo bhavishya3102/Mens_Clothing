01d62d5eabf728fb40fcf6b144fdd11f63a9de42
bhavishya <bhavisachdeva3102@gmail.com>
2025-03-15 15:16:19 +0530

    fix the build issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,8 +16,6 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { client } from "@/sanity/lib/client";
